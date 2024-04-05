4759fb45bdcab0155babca4aad6ecd535bda5e0e
Matheus-D-R-Costa <matheusdarosacosta@outlook.com>
2024-04-05 11:05:07 -0300

    add new java exercises

---
+++ Java/Notes/metodos/src/SmartTv.java
@@ -0,0 +1,30 @@
+public class SmartTv {
+    boolean power = false;
+    int channel = 1;
+    int volume = 25;
+
+    public void PowerOn() {
+        power = true;
+    }
+    public void PowerOff() {
+        power = false;
+    }
+
+    public void chooseChannel(int numberChannel) {
+        channel = numberChannel;
+    }
+    public void nextChannel() {
+        channel++;
+    }
+
+    public void previousChannel() {
+        channel--;
+    }
+    public void volumeUp() {
+        volume++;
+    }
+
+    public void volumeDown() {
+        volume--;
+    }
+}
--- Java/Notes/tipos-variaveis/src/TiposVariaveis.java
+++ Java/Notes/tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,10 @@
 public class TiposVariaveis {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 21070333;
+        long cpf = 98765432109L;
+        float pi = 3.14F;
+        double salario = 1275.33;
     }
 }
