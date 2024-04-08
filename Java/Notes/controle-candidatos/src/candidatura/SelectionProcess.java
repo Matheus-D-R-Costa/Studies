35ac65bd102fdf776b905d598f0e90702ff22a92
Matheus-D-R-Costa <matheusdarosacosta@outlook.com>
2024-04-08 12:33:15 -0300

    add new java files

---
+++ Java/Notes/controle-candidatos/src/candidatura/SelectionProcess.java
@@ -0,0 +1,13 @@
+package candidatura;
+
+public class SelectionProcess {
+
+    public static void main(String[] args) {
+        CandidatesAnalysis candidatesAnalysis = new CandidatesAnalysis();
+
+        candidatesAnalysis.selectCandidates();
+
+    }
+
+}
+
