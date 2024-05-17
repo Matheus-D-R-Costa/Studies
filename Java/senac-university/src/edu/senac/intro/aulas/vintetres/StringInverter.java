0775da171af347866a177cd25c3fb4ebadd685d7
Matheus-D-R-Costa <matheusdarosacosta@outlook.com>
2024-05-17 00:30:58 -0300

    add new files

---
+++ Java/senac-university/src/edu/senac/intro/aulas/vintetres/StringInverter.java
@@ -0,0 +1,26 @@
+package edu.senac.intro.aulas.vintetres;
+
+
+import java.util.Scanner;
+
+public class StringInverter {
+
+    static Scanner scanner = new Scanner(System.in);
+    static String word;
+    static String[] boxOfInvertedWords;
+    static String invertedString = "";
+
+    public static void main(String[] args) {
+
+        System.out.println("Digite a palavra que o senhor deseja inverter.");
+        word = scanner.next();
+        boxOfInvertedWords = new String[]{word};
+
+       for (int index = boxOfInvertedWords[0].length() - 1; index >= 0; index--) {
+           invertedString += boxOfInvertedWords[0].charAt(index);
+       }
+
+        System.out.println(invertedString);
+
+    }
+}
